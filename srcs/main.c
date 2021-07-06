dac6a612d08c28035379ecb9edc70de5e9bd071f
UnDesSix <mlarboul@student.42.fr>
2021-07-06 17:10:22 +0200

    all XPM OK + testing

--- srcs/main.c
+++ srcs/main.c
@@ -1,4 +1,5 @@
 #include <mlx.h>
+#include <stdio.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -8,27 +9,25 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+	char	*relative_path = "./xpm/water.xpm";
+	int		img_width;
+	int		img_height;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 960, 640, "Hello world!");
-	img.img = mlx_new_image(mlx, 960, 640);
+	mlx_win = mlx_new_window(mlx, 960, 540, "POKOMEN");
+	img.img = mlx_new_image(mlx, 960, 540);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
+	img.img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	img.img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 32, 32);
 	mlx_loop(mlx);
+	return (0);
 }
 
