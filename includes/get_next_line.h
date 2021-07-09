d6e7c093ca89b2fcf3cc770c9f81e32edb5ef051
UnDesSix <mlarboul@student.42.fr>
2021-07-09 09:40:40 +0200

    Headers normed

---
+++ includes/get_next_line.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 18:30:23 by mlarboul          #+#    #+#             */
+/*   Updated: 2021/07/09 09:40:21 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFFER_SIZE 1024
+
+typedef unsigned int	t_size;
+
+typedef struct s_lista
+{
+	int				fd;
+	int				flag_eof;
+	t_size			buff_size;
+	t_size			line_size;
+	t_size			buff_indx;
+	char			*buffer;
+	struct s_list	*next;
+}				t_lista;
+
+int		get_next_line(int fd, char **line);
+int		ft_read_fd(t_lista *elm, char **line);
+int		ft_fill_line(t_lista *elm, char **line);
+void	ft_clear_elm(t_lista **begin_list, t_lista *elm_to_del);
+t_lista	*ft_create_elmem(int fd);
+t_lista	*ft_push_front(t_lista **begin_list, int fd);
+char	*ft_strncpy(char *dst, char *src, size_t len);
+char	*ft_realloc_spe(char *src, size_t init_len, size_t add_len);
+char	*ft_strncat(char *dst, char *src,
+			size_t line_offset, size_t add_len);
+
+#endif
---
+++ includes/so_long.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/09 09:36:36 by mlarboul          #+#    #+#             */
+/*   Updated: 2021/07/09 09:39:35 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <mlx.h>
+# include "get_next_line.h"
+
+# define WIDTH	256
+# define HEIGHT	256
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+#endif
