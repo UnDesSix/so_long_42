378e44577af88c3c7c546e6f4e175be86cd3293f
UnDesSix <mlarboul@student.42.fr>
2021-07-09 12:47:05 +0200

    test OK

--- srcs/main.c
+++ srcs/main.c
@@ -1,19 +1,16 @@
-#include <mlx.h>
-#include <stdio.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/09 09:33:57 by mlarboul          #+#    #+#             */
+/*   Updated: 2021/07/09 12:44:58 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define WIDTH	256
-#define HEIGHT	256
-
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
+#include "../includes/so_long.h"
 
 void	fill_final_image(char *final, char *water, int w, int h)
 {
@@ -36,35 +33,45 @@ void	fill_final_image(char *final, char *water, int w, int h)
 
 int	main(void)
 {
-	char 	map[] = "2333333450000007500000075000000750000007500000075000000769999998";
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	t_data	water;
 	t_data	ground;
-
+	t_data	wall;
+	t_data	water;
+	t_data	collect;
+	t_data	exit;
+	
 	t_data	top_l;
 	t_data	top_r;
 	t_data	bot_l;
 	t_data	bot_r;
+	
 	t_data	top;
 	t_data	bot;
 	t_data	left;
 	t_data	right;
 	
 	char	*ground_p = "./xpm/ground.xpm";
-	char	*water_p = "./xpm/water.xpm";
+	char	*collect_p = "./xpm/collectible.xpm";
+	char	*wall_p = "./xpm/wall.xpm";
+	char	*exit_p = "./xpm/exit.xpm";
+	
 	char	*top_l_p = "./xpm/top_left.xpm";
 	char	*top_r_p = "./xpm/top_right.xpm";
 	char	*bot_l_p = "./xpm/bot_left.xpm";
 	char	*bot_r_p = "./xpm/bot_right.xpm";
+	
 	char	*top_p = "./xpm/top.xpm";
 	char	*bot_p = "./xpm/bot.xpm";
 	char	*left_p = "./xpm/left.xpm";
 	char	*right_p = "./xpm/right.xpm";
+	
 	int		img_width = 0;
 	int		img_height = 0;
 	int		i;
+	t_map	*test;
+	char	*map;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "PEKOMEN");
@@ -72,14 +79,24 @@ int	main(void)
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 
-	water.img = mlx_xpm_file_to_image(mlx, water_p, &img_width, &img_height);
-	water.addr = mlx_get_data_addr(water.img, &water.bits_per_pixel, &water.line_length,
-								&water.endian);
-
+	// GROUND, WALL, COLLECT, EXIT
 	ground.img = mlx_xpm_file_to_image(mlx, ground_p, &img_width, &img_height);
 	ground.addr = mlx_get_data_addr(ground.img, &water.bits_per_pixel, &water.line_length,
 								&water.endian);
 
+	wall.img = mlx_xpm_file_to_image(mlx, wall_p, &img_width, &img_height);
+	wall.addr = mlx_get_data_addr(wall.img, &water.bits_per_pixel, &water.line_length,
+								&water.endian);
+
+	collect.img = mlx_xpm_file_to_image(mlx, collect_p, &img_width, &img_height);
+	collect.addr = mlx_get_data_addr(collect.img, &water.bits_per_pixel, &water.line_length,
+								&water.endian);
+
+	exit.img = mlx_xpm_file_to_image(mlx, exit_p, &img_width, &img_height);
+	exit.addr = mlx_get_data_addr(exit.img, &water.bits_per_pixel, &water.line_length,
+								&water.endian);
+
+	// WATER WALL
 	top_l.img = mlx_xpm_file_to_image(mlx, top_l_p, &img_width, &img_height);
 	top_l.addr = mlx_get_data_addr(top_l.img, &water.bits_per_pixel, &water.line_length,
 								&water.endian);
@@ -111,44 +128,44 @@ int	main(void)
 	right.img = mlx_xpm_file_to_image(mlx, right_p, &img_width, &img_height);
 	right.addr = mlx_get_data_addr(right.img, &water.bits_per_pixel, &water.line_length,
 								&water.endian);
+
+	test = read_map("test.ber");
+	check_map(test);
+	convert_map(test);
+	map = test->content;
 	
 	i = 0;
 	while (map[i] != 0)
 	{
-		if (map[i] == '2')
+		if (map[i] == 'z')
 			fill_final_image(img.addr, top_l.addr, i % 8, i / 8);
-		else if (map[i] == '4')
+		else if (map[i] == 'x')
 			fill_final_image(img.addr, top_r.addr, i % 8, i / 8);
-		else if (map[i] == '6')
+		else if (map[i] == 'c')
 			fill_final_image(img.addr, bot_l.addr, i % 8, i / 8);
-		else if (map[i] == '8')
+		else if (map[i] == 'v')
 			fill_final_image(img.addr, bot_r.addr, i % 8, i / 8);
-		else if (map[i] == '3')
+		else if (map[i] == 'T')
 			fill_final_image(img.addr, top.addr, i % 8, i / 8);
-		else if (map[i] == '5')
+		else if (map[i] == 'L')
 			fill_final_image(img.addr, left.addr, i % 8, i / 8);
-		else if (map[i] == '7')
+		else if (map[i] == 'R')
 			fill_final_image(img.addr, right.addr, i % 8, i / 8);
-		else if (map[i] == '9')
+		else if (map[i] == 'B')
 			fill_final_image(img.addr, bot.addr, i % 8, i / 8);
+		else if (map[i] == 'C')
+			fill_final_image(img.addr, collect.addr, i % 8, i / 8);
+		else if (map[i] == 'w')
+			fill_final_image(img.addr, wall.addr, i % 8, i / 8);
+		else if (map[i] == 'E')
+			fill_final_image(img.addr, exit.addr, i % 8, i / 8);
 		else
 			fill_final_image(img.addr, ground.addr, i % 8, i / 8);
 		i++;
 	}
 
-/*
-	fill_final_image(img.addr, water.addr, 0, 0);
-	fill_final_image(img.addr, water.addr, 1, 0);
-	fill_final_image(img.addr, water.addr, 2, 0);
-	fill_final_image(img.addr, water.addr, 3, 0);
-	fill_final_image(img.addr, water.addr, 4, 0);
-	fill_final_image(img.addr, water.addr, 5, 0);
-	fill_final_image(img.addr, water.addr, 6, 0);
-	fill_final_image(img.addr, water.addr, 7, 0);
-	fill_final_image(img.addr, water.addr, 8, 0);
-	fill_final_image(img.addr, water.addr, 9, 0);
-	fill_final_image(img.addr, water.addr, 0, 1);
-*/
+	free(map);
+	free(test);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	mlx_destroy_image(mlx, water.img);
