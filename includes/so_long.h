c7257970c8947ad95ba259d97e51c929d45d52bb
UnDesSix <mlarboul@student.42.fr>
2021-07-10 14:17:40 +0200

    transition is not that easy

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 09:36:36 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/07/09 19:29:13 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/07/10 14:09:27 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,12 @@
 # include "get_next_line.h"
 # include "../libft/libft.h"
 
-# define WIDTH	256
-# define HEIGHT	256
+# define WW		20
+# define HH		20
+
+# define WIDTH	(WW * 32)
+# define HEIGHT	(HH * 32)
+
 
 //# define ARROW_LEFT		65361
 # define ARROW_LEFT		97
@@ -62,13 +66,38 @@ typedef struct s_player
 	char	*wlk_2;
 }				t_player;
 
+typedef struct s_sprite
+{
+	t_data	*img;
+	t_data	*ground;
+	t_data	*player;
+	t_data	*wall;
+	t_data	*water;
+	t_data	*collect;
+	t_data	*exit;
+
+	t_data	*top_l;
+	t_data	*top_r;
+	t_data	*bot_l;
+	t_data	*bot_r;
+
+	t_data	*top;
+	t_data	*bot;
+	t_data	*left;
+	t_data	*right;
+}				t_sprite;
+
 typedef struct s_saver
 {
 	void		*mlx;
 	void		*mlx_win;
-	t_data		*img;
-	t_data		map_saved;
+	t_data		img;
+	char		*saved_map;
 	char		*map_ascii;
+	t_map		*map;
+	t_sprite	*sprt;
+
+	int		i;
 
 	char		*ground;
 	t_player	front;
@@ -77,6 +106,7 @@ typedef struct s_saver
 	t_player	right;
 }				t_saver;
 
+
 t_map	*read_map(char *map_file);
 void	check_map(t_map *map);
 void	convert_map(t_map *map);
@@ -84,5 +114,10 @@ void	convert_map(t_map *map);
 int		key_manager(int keycode, t_saver *saver);
 
 void	fill_player(char *map, char *player, int w, int h);
+void	fill_player_trans(char *map, char *player, int w, int h, int offset);
 void	fill_final_image(char *map, char *player, int w, int h);
+void	fill_map(char *map_ascii, t_map *map, t_sprite *sprt);
+
+int		right_trans(t_saver *saver);
+
 #endif
